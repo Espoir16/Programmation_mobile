5b4de316525f07116412d3b6048326116c87dd77
Espoir <mahouna.hessou@edu.ece.fr>
2026-02-01 15:57:42 +0100

    ajout du dossier widget et premier fichier

---
+++ cours_ece_2026-main_cours_02/cours_ece_2026/lib/widgets/product_provider.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:formation_flutter/model/product.dart';
+
+class ProductProvider extends InheritedWidget {
+  final Product product;
+
+  const ProductProvider({
+    super.key,
+    required this.product,
+    required Widget child,
+  }) : super(child: child);
+
+  static Product of(BuildContext context) {
+    final provider =
+        context.dependOnInheritedWidgetOfExactType<ProductProvider>();
+    assert(provider != null, 'No ProductProvider found in context');
+    return provider!.product;
+  }
+
+  @override
+  bool updateShouldNotify(ProductProvider oldWidget) =>
+      product != oldWidget.product;
+}
