ac4608f8116fa168c816b8b7b504132279c8f5a0
Espoir <mahouna.hessou@edu.ece.fr>
2026-02-16 09:27:05 +0100

    Ajout exo4

---
+++ Exo_fait_en_classe/cours_ece_2026-main_cours_04/cours_ece_2026/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\hesso\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\hesso\OneDrive - Groupe INSEEC (POCE)\Bureau\ING4\Prog_mobile\Programmation_mobile\Exo_fait_en_classe\cours_ece_2026-main_cours_04\cours_ece_2026"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
